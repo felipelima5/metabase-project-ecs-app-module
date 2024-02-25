2e667564b01c52b7a5952c77085637f08aa6f83d
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-02-25 16:16:28 -0300

    add iam policy document with permission to execute task at ecs

--- iam_execution_role.tf
+++ iam_execution_role.tf
@@ -1,3 +1,41 @@
+#Policy
+data "aws_iam_policy_document" "ecs_task_policy" {
+
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "s3:GetBucketLocation",
+      "kms:Decrypt",
+      "secretsmanager:GetSecretValue",
+      "ssm:*"
+    ]
+
+    effect = "Allow"
+    resources = [
+      "*"
+    ]
+
+    sid = "PermissionsTaskECS"
+  }
+
+  statement {
+    actions = [
+      "s3:*"
+    ]
+
+    effect = "Allow"
+    resources = [
+      "arn:aws:s3:::${var.bucket_env_name}/*"
+    ]
+
+    sid = "s3GetObject"
+  }
+}
 
 
 resource "random_password" "ecs_task_role_name_sufixo" {
@@ -9,3 +47,4 @@ resource "random_password" "ecs_task_role_name_sufixo" {
   numeric     = true
   min_numeric = 10
 }
+
